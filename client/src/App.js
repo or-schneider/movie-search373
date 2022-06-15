e8b2942eb614347114bc7635ad0ef17b229ef560
Or S <orschneider@protonmail.com>
2022-06-15 20:53:01 +0300

    Sets up a base create react app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import PopularMovies from "./Features/PopularMovies/PopularMovies.jsx";
+
+function App() {
+  return (
+    <div>
+      App
+      <PopularMovies></PopularMovies>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Features/PopularMovies/PopularMovies.jsx
@@ -0,0 +1,7 @@
+import style from "./PopularMovies.module.css";
+
+function PopularMovies() {
+  return <div className={style.root}>Popular Movies</div>;
+}
+
+export default PopularMovies;
