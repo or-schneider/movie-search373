82a523d08a6904fd496db9bedec8b710591502d5
Or S <orschneider@protonmail.com>
2022-06-16 05:36:06 +0300

    Extracts movies api url to .env

--- client/src/Features/PopularMovies/usePopularMovies.jsx
+++ client/src/Features/PopularMovies/usePopularMovies.jsx
@@ -1,15 +1,14 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const apiBaseUrl = process.env.REACT_APP_MOVIES_API_BASE_URL;
 function usePopularMovies() {
   const [popularMovies, setPopularMovies] = useState([]);
   const [error, setError] = useState(null);
   useEffect(() => {
     (async () => {
       try {
-        const movies = await axios.get(
-          "http://localhost:8000/api/movies/popular"
-        );
+        const movies = await axios.get(`${apiBaseUrl}/movies/popular`);
         setPopularMovies(movies.data);
       } catch (error) {
         console.log(error); //TODO
